0b688aae8c5a9dc4f693465ee0f8733e7bcfb557
Amin Alaee <mohammadamin.alaee@gmail.com>
2025-12-22 11:28:23 +0000

    Add SQS data source attributes

--- aws_lambda_opentelemetry/utils.py
+++ aws_lambda_opentelemetry/utils.py
@@ -1,3 +1,4 @@
+import enum
 import os
 
 from opentelemetry.semconv._incubating.attributes.cloud_attributes import (
@@ -15,6 +16,34 @@ from opentelemetry.semconv._incubating.attributes.faas_attributes import (
     FaasInvokedProviderValues,
     FaasTriggerValues,
 )
+from opentelemetry.semconv._incubating.attributes.messaging_attributes import (
+    MESSAGING_BATCH_MESSAGE_COUNT,
+    MESSAGING_DESTINATION_NAME,
+    MESSAGING_OPERATION,
+    MESSAGING_SYSTEM,
+    MessagingOperationTypeValues,
+)
+
+# from opentelemetry.semconv.attributes.http_attributes import (
+#     HTTP_REQUEST_METHOD,
+#     HTTP_ROUTE,
+# )
+# from opentelemetry.semconv.attributes.server_attributes import (
+#     SERVER_ADDRESS,
+# )
+# from opentelemetry.semconv.attributes.url_attributes import (
+#     URL_PATH,
+#     URL_SCHEME,
+# )
+# from opentelemetry.semconv.attributes.user_agent_attributes import (
+#     USER_AGENT_ORIGINAL,
+# )
+# from opentelemetry.semconv.attributes.client_attributes import (
+#     CLIENT_ADDRESS,
+# )
+# from opentelemetry.semconv.attributes.network_attributes import (
+#     NETWORK_PROTOCOL_VERSION,
+# )
 from opentelemetry.trace import Span
 
 from aws_lambda_opentelemetry import constants
@@ -23,11 +52,28 @@ from aws_lambda_opentelemetry.typing.context import LambdaContext
 _is_cold_start = True
 
 
-def set_lambda_handler_attributes(event: dict, context: LambdaContext, span: Span):
+class AwsDataSource(enum.Enum):
+    API_GATEWAY = "aws.api_gateway"
+    HTTP_API = "aws.http_api"
+    ELB = "aws.elb"
+    SQS = "aws.sqs"
+    SNS = "aws.sns"
+    S3 = "aws.s3"
+    DYNAMODB = "aws.dynamodb"
+    KINESIS = "aws.kinesis"
+    EVENT_BRIDGE = "aws.event_bridge"
+    CLOUDWATCH_LOGS = "aws.cloudwatch_logs"
+    OTHER = "aws.other"
+
+
+def set_handler_attributes(event: dict, context: LambdaContext, span: Span):
     """
     Set standard AWS Lambda attributes on the given span.
     """
 
+    data_source_mapper = DataSourceAttributeMapper(event)
+
+    span.set_attributes(data_source_mapper.attributes)
     span.set_attributes(
         {
             FAAS_INVOCATION_ID: context.aws_request_id,
@@ -37,45 +83,98 @@ def set_lambda_handler_attributes(event: dict, context: LambdaContext, span: Spa
             FAAS_MAX_MEMORY: context.memory_limit_in_mb,
             FAAS_VERSION: context.function_version,
             FAAS_COLDSTART: _check_cold_start(),
-            FAAS_TRIGGER: get_lambda_datasource(event).value,
+            FAAS_TRIGGER: data_source_mapper.faas_trigger.value,
             CLOUD_RESOURCE_ID: context.invoked_function_arn,
         }
     )
 
 
-def get_lambda_datasource(event: dict) -> FaasTriggerValues:
-    """
-    Extract the data source from the Lambda event.
-    """
+class DataSourceAttributeMapper:
+    def __init__(self, event: dict):
+        self.event = event
+        self.data_source, self.faas_trigger = self.get_sources()
 
-    # HTTP triggers
-    http_keys = ["apiId", "http", "elb"]
-    if "requestContext" in event:
-        if any(key in event["requestContext"] for key in http_keys):
-            return FaasTriggerValues.HTTP
+    @property
+    def attributes(self) -> dict:
+        if self.data_source == AwsDataSource.SQS:
+            return self._get_sqs_attributes()
+        # if self.data_source == AwsDataSource.SNS:
+        #     ...
+        # if self.data_source == AwsDataSource.S3:
+        #     ...
+        # if self.data_source == AwsDataSource.DYNAMODB:
+        #     ...
+        # if self.data_source == AwsDataSource.KINESIS:
+        #     ...
+        # if self.data_source == AwsDataSource.EVENT_BRIDGE:
+        #     ...
+        # if self.data_source == AwsDataSource.CLOUDWATCH_LOGS:
+        #     ...
+        # if self.data_source == AwsDataSource.API_GATEWAY:
+        #     ...
+        # if self.data_source == AwsDataSource.HTTP_API:
+        #     ...
+        # if self.data_source == AwsDataSource.ELB:
+        #     ...
+        return {}
 
-    # EventBridge
-    if "source" in event and "detail-type" in event:
-        if event["detail-type"] == "Scheduled Event":
-            return FaasTriggerValues.TIMER
-        return FaasTriggerValues.PUBSUB
+    def get_sources(self) -> tuple[AwsDataSource, FaasTriggerValues]:
+        # HTTP triggers
+        if "requestContext" in self.event:
+            if "apiId" in self.event["requestContext"]:
+                return (AwsDataSource.API_GATEWAY, FaasTriggerValues.HTTP)
 
-    # SNS/SQS/S3/DynamoDB/Kinesis
-    if "Records" in event and len(event["Records"]) > 0:
-        record = event["Records"][0]
-        event_source = record.get("eventSource")
+            if "http" in self.event["requestContext"]:
+                return (AwsDataSource.HTTP_API, FaasTriggerValues.HTTP)
 
-        if event_source in {"aws:sns", "aws:sqs"}:
-            return FaasTriggerValues.PUBSUB
+            if "elb" in self.event["requestContext"]:
+                return (AwsDataSource.ELB, FaasTriggerValues.HTTP)
 
-        if event_source in {"aws:s3", "aws:dynamodb", "aws:kinesis"}:
-            return FaasTriggerValues.DATASOURCE
+        # EventBridge
+        if "source" in self.event and "detail-type" in self.event:
+            if self.event["detail-type"] == "Scheduled Event":
+                return (AwsDataSource.EVENT_BRIDGE, FaasTriggerValues.TIMER)
+            return (AwsDataSource.EVENT_BRIDGE, FaasTriggerValues.PUBSUB)
 
-    # CloudWatch Logs
-    if "awslogs" in event and "data" in event["awslogs"]:
-        return FaasTriggerValues.DATASOURCE
+        # SNS/SQS/S3/DynamoDB/Kinesis
+        if "Records" in self.event and len(self.event["Records"]) > 0:
+            record = self.event["Records"][0]
+            event_source = record.get("eventSource")
 
-    return FaasTriggerValues.OTHER
+            if event_source == "aws:sns":
+                return (AwsDataSource.SNS, FaasTriggerValues.PUBSUB)
+
+            if event_source == "aws:sqs":
+                return (AwsDataSource.SQS, FaasTriggerValues.PUBSUB)
+
+            if event_source == "aws:s3":
+                return (AwsDataSource.S3, FaasTriggerValues.DATASOURCE)
+
+            if event_source == "aws:dynamodb":
+                return (AwsDataSource.DYNAMODB, FaasTriggerValues.DATASOURCE)
+
+            if event_source == "aws:kinesis":
+                return (AwsDataSource.KINESIS, FaasTriggerValues.DATASOURCE)
+
+        # CloudWatch Logs
+        if "awslogs" in self.event and "data" in self.event["awslogs"]:
+            return (AwsDataSource.CLOUDWATCH_LOGS, FaasTriggerValues.DATASOURCE)
+
+        return (AwsDataSource.OTHER, FaasTriggerValues.OTHER)
+
+    def _get_sqs_attributes(self) -> dict:
+        records = self.event.get("Records", [])
+        message_count = len(records)
+        queue_arn = records[0].get("eventSourceARN", "") if message_count > 0 else ""
+        queue_name = queue_arn.split(":")[-1]
+
+        return {
+            MESSAGING_SYSTEM: self.data_source.value,
+            MESSAGING_OPERATION: MessagingOperationTypeValues.RECEIVE.value,
+            MESSAGING_BATCH_MESSAGE_COUNT: message_count,
+            MESSAGING_DESTINATION_NAME: queue_name,
+            CLOUD_RESOURCE_ID: queue_arn,
+        }
 
 
 def _check_cold_start() -> bool:
