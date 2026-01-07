373afeb0709b76245de85417e8192f366e2ba13c
Amin Alaee <amin.alaee.zaviyeh@postnl.nl>
2026-01-07 21:49:43 +0100

    update module imports

---
+++ aws_lambda_opentelemetry/__init__.py
@@ -0,0 +1,5 @@
+from aws_lambda_opentelemetry.trace import instrument_handler
+
+__all__ = [
+    "instrument_handler",
+]
---
+++ aws_lambda_opentelemetry/typing/__init__.py
@@ -0,0 +1,5 @@
+from aws_lambda_opentelemetry.typing.context import LambdaContext
+
+__all__ = [
+    "LambdaContext",
+]
